ce0346ae6da469265be124606ef49389b63f9d30
belainehi <belainehi@berea.edu>
2020-06-13 01:36:59 -0400

    Created Movie and followed along with the rest of the video.

---
+++ app/src/main/java/com/immanuela/flixter/adapters/MovieAdapter.java
@@ -0,0 +1,97 @@
+package com.immanuela.flixter.adapters;
+
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.immanuela.flixter.Movie;
+import com.immanuela.flixter.R;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
+
+    Context context;
+    List<Movie> movies;
+    String TAG = "MovieAdapter";
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+    // Usually involves inflating a layout from XML and returning the holder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter", "onCreateViewHolder");
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(movieView);
+    }
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter", "onBindViewHolder"+ position);
+        //Get movie at the passed in position
+        Movie movie = movies.get(position);
+        //Bind movie data into the VH
+        holder.bind(movie);
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        RelativeLayout container;
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
+        }
+
+        public void bind(final Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            String imageUrl;
+            //if phone is in landscape
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                //then imageUrl = back drop image
+                imageUrl = movie.getBackdropPath();
+            }else {
+                //else imageUrl = poster image
+                int rating = (int) movie.getRating();
+                // The measure of popularity is 90 or above for picking the poster type.
+                if (rating > 90) {
+                    imageUrl = movie.getBackdropPath();
+                    Log.i(TAG, "This is a popular movie so backdrop poster. ");
+                } else {
+                    imageUrl = movie.getPosterPath();
+                    Log.i(TAG, "This is not that popular so just poster. ");
+                }
+            }
+            Glide.with(context).load(imageUrl).into (ivPoster);
+
+
+        }
+    }
+}
